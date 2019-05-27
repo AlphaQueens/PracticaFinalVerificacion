461351e79a02962e36c210b240b79a226bf891e0
pablo <pablo.rubio@live.u-tad.com>
2019-05-27 22:34:16 +0200

    Actualizacion Travis

--- Verificacion/features/environment.py
+++ Verificacion/features/environment.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 def before_all(context):
-    context.browser = webdriver.Firefox(executable_path= r'C:\Django projects\Prueba\Verificacion\geckodriver\geckodriver.exe')
+    context.browser = webdriver.Firefox()
     context.browser.implicitly_wait(1)
     context.server_url = 'http://127.0.0.1:8000/'
 
